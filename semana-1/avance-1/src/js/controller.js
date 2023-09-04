a85e460072f829c44e700305473918944e0f2641
Alan Jimenez <alanjq@outlook.com>
2023-09-04 18:44:48 -0500

    Inciso 15

--- semana-1/avance-1/src/js/controller.js
+++ semana-1/avance-1/src/js/controller.js
@@ -11,3 +11,33 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+//#region Avance de proyecto 1. Inciso 15
+const urlapi = "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886"
+
+// La función que pide el PDF es similar a la siguiente
+async function showRecipe(api_url) {
+  return await fetch(api_url)
+    .then((response) => response.ok ? response.json() : Promise.reject(response.statusText))
+    // Inciso J
+    .then(response => response.status === 'success' ? response.data : Promise.reject(response))
+    // Inciso D
+    .catch((error) => error)
+}
+
+// Una alternativa puede ser la siguiente:
+const showRecipe2 = (api_url) => fetch(api_url)
+  .then((response) => response.ok ? response.json() : Promise.reject(response.statusText))
+  // Inciso J
+  .then(response => response.status === 'success' ? response.data : Promise.reject(response))
+  // Inciso D
+  .catch((error) => error)
+//#endregion / Avance de proyecto 1. Inciso 15
+
+showRecipe(urlapi).then((r) => {
+  console.log('response', r);
+})
+
+showRecipe2(urlapi).then((r) => {
+  console.log('response 2', r);
+})
